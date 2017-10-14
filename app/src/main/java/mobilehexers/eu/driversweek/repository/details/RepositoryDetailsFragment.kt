5abb9e37b20ba551ed846c9b601de33479e49927
Maciej Imiela <maciej.imiela.dev@gmail.com>
2017-10-14 13:40:52 +0200

    task/36 Update RepositoryDetailsFragment to display mocked data

--- app/src/main/java/mobilehexers/eu/driversweek/repository/details/RepositoryDetailsFragment.kt
+++ app/src/main/java/mobilehexers/eu/driversweek/repository/details/RepositoryDetailsFragment.kt
@@ -11,6 +11,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_description_text
+import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_id_text
+import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_language_text
+import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_name_text
 import mobilehexers.eu.driversweek.R
 import mobilehexers.eu.driversweek.extensions.inflate
 import mobilehexers.eu.presentation.repository.workflow.RepositoryWorkflow
@@ -19,6 +23,10 @@ import javax.inject.Inject
 class RepositoryDetailsFragment : Fragment() {
 
     @Inject lateinit var workflow: RepositoryWorkflow
+    private val idTextView by lazy { repository_details_id_text }
+    private val nameTextView by lazy { repository_details_name_text }
+    private val descriptionTextView by lazy { repository_details_description_text }
+    private val languageTextView by lazy { repository_details_language_text }
 
     companion object {
         fun newInstance(): RepositoryDetailsFragment {
@@ -39,6 +47,16 @@ class RepositoryDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //        initView()
+        initView()
     }
+
+    private fun initView() {
+        val item = createMockedRepositoryDetailsItem()
+        idTextView.text=item.id
+        nameTextView.text = item.name
+        descriptionTextView.text = item.description
+        languageTextView.text = item.language
+    }
+
+    private fun createMockedRepositoryDetailsItem() = RepositoryDetailsItem("01", "retrofit", "Type-safe HTTP client for Android and Java by Square, Inc.", "Java")
 }
---
+++ app/src/main/java/mobilehexers/eu/driversweek/repository/details/RepositoryDetailsItem.kt
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2017.  All rights reserved - Maciej Imiela.
+ */
+
+package mobilehexers.eu.driversweek.repository.details
+
+data class RepositoryDetailsItem(val id: String, val name: String, val description: String, val language: String)
