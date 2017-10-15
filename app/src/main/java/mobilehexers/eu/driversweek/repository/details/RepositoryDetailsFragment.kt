be4447625e19aa580c4c18eb32358c59788f2a2f
Maciej Imiela <maciej.imiela.dev@gmail.com>
2017-10-15 21:42:29 +0200

    Changed Fragment details to use Android Binding

--- app/src/main/java/mobilehexers/eu/driversweek/repository/details/RepositoryDetailsFragment.kt
+++ app/src/main/java/mobilehexers/eu/driversweek/repository/details/RepositoryDetailsFragment.kt
@@ -8,14 +8,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_description_text
-import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_id_text
 import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_item_container
-import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_language_text
-import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_name_text
 import kotlinx.android.synthetic.main.fragment_repository_detail.repository_details_progressbar_container
-import mobilehexers.eu.driversweek.R
-import mobilehexers.eu.driversweek.extensions.inflate
+import mobilehexers.eu.driversweek.databinding.FragmentRepositoryDetailBinding
 import mobilehexers.eu.driversweek.repository.manager.RepositoryManager
 import mobilehexers.eu.driversweek.repository.model.RepositoryModel
 import mobilehexers.eu.presentation.repository.workflow.RepositoryWorkflow
@@ -28,10 +23,8 @@ class RepositoryDetailsFragment : BaseFragment() {
     @Inject lateinit var repositoryManager: RepositoryManager
     @Inject lateinit var model: RepositoryModel
 
-    private val idTextView by lazy { repository_details_id_text }
-    private val nameTextView by lazy { repository_details_name_text }
-    private val descriptionTextView by lazy { repository_details_description_text }
-    private val languageTextView by lazy { repository_details_language_text }
+    private val details = RepositoryDetailsItem()
+    private lateinit var binding: FragmentRepositoryDetailBinding
 
     companion object {
         fun newInstance(): RepositoryDetailsFragment {
@@ -42,21 +35,21 @@ class RepositoryDetailsFragment : BaseFragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? = container?.inflate(
-            R.layout.fragment_repository_detail)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentRepositoryDetailBinding.inflate(inflater)
+        binding.details = details
+        return binding.root
+    }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         addDisposable(repositoryManager.getRepositoryDetail(repositoryName = model.repositoryItemClicked.name).subscribe(
                 { next -> updateViewWith(next); showDataAndHideProgress() }))
     }
 
     private fun updateViewWith(item: RepositoryDetailsItem) {
-        idTextView.text = item.id
-        nameTextView.text = item.name
-        descriptionTextView.text = item.description
-        languageTextView.text = item.language
+        binding.details = item
     }
 
     private fun showDataAndHideProgress() {
